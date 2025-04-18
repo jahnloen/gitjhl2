e24b6e04548dac1480804ee124ea9f5982e0b210
jahnloen <jahnloen@gmail.com>
2025-04-18 13:06:49 +0200

    name based on workspaces

--- oving7/terraform/main.tf
+++ oving7/terraform/main.tf
@@ -1,4 +1,18 @@
-resource "azurerm_resource_group" "rgname" {
-  name = var.rg_name
+locals {
+  workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}" #om workspace ikke er default, skal navneendelse tilsvare navnet på nåværende workspace
+
+  rg_name = "${var.rg_name}-${local.workspaces_suffix}" #legger til feks dev bak navnet på rg
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = local.rg_name
   location = var.rg_location
 }
+
+output "rg_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+#terraform workspace list - lister alle workspaces som er tilgjengeligte
+#terraform workspace new prod/test/dev - nye workspace for å separere miljø
+#terraform workspace select dev
