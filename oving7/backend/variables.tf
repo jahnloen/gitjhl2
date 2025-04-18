cee7f38691b215ff100cf26add23a8e7e87bc547
jahnloen <jahnloen@gmail.com>
2025-04-18 11:22:55 +0200

    be snf tf

---
+++ oving7/backend/variables.tf
@@ -0,0 +1,25 @@
+variable "sa_backend_name" {
+  type        = string
+  description = ""
+}
+
+variable "sc_backend_name" {
+  type = string
+}
+
+variable "rg_backend_name" {
+  type = string
+}
+
+variable "kv_backend_name" {
+  type = string
+}
+
+variable "rg_backend_location" {
+  type        = string
+  description = ""
+}
+
+variable "sa_backend_accesskey_name" {
+  type = string
+}
---
+++ oving7/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+    type = string
+ default = "rg_name_jhl" 
+}
+
+variable "rg_location" {
+  type = string
+  default = "westeurope"
+}
