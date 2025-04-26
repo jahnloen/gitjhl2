f620cab0d21401c35f0594c3354894ae8932dfa4
jahnloen <jahnloen@gmail.com>
2025-04-26 16:17:53 +0200

    fjernet gamle versjoner

--- oving4/1.3/outputs.tf
+++
@@ -1,7 +0,0 @@
-#egen fil for outputs
-#kan brukes til å vise verdier for variabler og beregninger når "terraform apply" er kjørt
-
-output "storage_account_names" {
-  description = "name of created storage accounts"
-  value       = var.storage_account_names
-}
--- oving4/1.4/networking.tf
+++
@@ -1,2 +0,0 @@
-#ressursfil for nettverk
-#i sammenheng med locals kan locals verdier brukes i ressursfiler for å beholde oversikt, gjenbruk, vedlikehold og konsistent kode i større organisasjoner
