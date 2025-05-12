e08249c74b0c7b71a26c78681b75c1efcd03fd91
jahnloen <jahnloen@gmail.com>
2025-05-12 19:52:35 +0200

    00

--- oving9/infra/network.tf
+++ oving9/infra/network.tf
@@ -8,7 +8,7 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_network_security_rule" "nsgrule" {
   name                        = "RDP_SSH"
-  priority                    = 104
+  priority                    = 105
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
